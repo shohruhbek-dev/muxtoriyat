084783f3889098b917b414df164c4012bbf0e303
Zufarbek2002 <game123qwerty00@gmail.com>
2025-05-12 13:09:48 +0500

    fixed counter

--- src/components/Sectios/countersSec/index.jsx
+++ src/components/Sectios/countersSec/index.jsx
@@ -2,21 +2,52 @@ import clsx from "clsx";
 import Counter from "../../Card/counter";
 import cn from "./style.module.scss";
 import { useTranslation } from "react-i18next";
+import useInView from "../../../hooks/useInView";
+
 function CountersSec() {
   const { t } = useTranslation();
+  const [ref, isInView] = useInView({ threshold: 0.3 });
+
   return (
-    <div className={clsx(cn["bigcounters"])}>
+    <div className={clsx(cn["bigcounters"])} ref={ref}>
       <div className={clsx(cn["countersText"])}>
         <h1>{t("countersSecHeader")}</h1>
-
         <p>{t("countersSecSub")}</p>
       </div>
 
       <div className={clsx(cn["counters"])}>
-        <Counter start={0} end={54} duration={1.5} text={t("articles")} />
-        <Counter start={0} end={47} duration={1.5} text={t("books")} />
-        <Counter start={0} end={16} duration={1.5} text={t("historyDoc")} />
-        <Counter start={0} end={21} duration={1.5} text={t("autoRefer")} />
+        {isInView && (
+          <>
+            <Counter
+              key={isInView + "-articles"}
+              start={0}
+              end={54}
+              duration={1.5}
+              text={t("articles")}
+            />
+            <Counter
+              key={isInView + "-books"}
+              start={0}
+              end={47}
+              duration={1.5}
+              text={t("books")}
+            />
+            <Counter
+              key={isInView + "-historyDoc"}
+              start={0}
+              end={16}
+              duration={1.5}
+              text={t("historyDoc")}
+            />
+            <Counter
+              key={isInView + "-autoRefer"}
+              start={0}
+              end={21}
+              duration={1.5}
+              text={t("autoRefer")}
+            />
+          </>
+        )}
       </div>
     </div>
   );
---
+++ src/hooks/useInView.js
@@ -0,0 +1,24 @@
+// hooks/useInView.js
+import { useEffect, useState, useRef } from "react";
+
+const useInView = (options) => {
+  const ref = useRef(null);
+  const [isInView, setIsInView] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsInView(entry.isIntersecting);
+    }, options);
+
+    const currentRef = ref.current;
+    if (currentRef) observer.observe(currentRef);
+
+    return () => {
+      if (currentRef) observer.unobserve(currentRef);
+    };
+  }, [ref, options]);
+
+  return [ref, isInView];
+};
+
+export default useInView;
